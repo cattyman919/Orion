ef28dcb77e3417173be8e9c2caba0c46a0aaed0f
Cattyman <88530324+cattyman919@users.noreply.github.com>
2022-10-29 00:48:19 +0700

    send pure json data

--- controllers/projectControllers.js
+++ controllers/projectControllers.js
@@ -3,7 +3,8 @@ const Project = require("../models/Project");
 exports.getAllProjects = async (req, res, next) => {
   try {
     const [projects, _] = await Project.findAll();
-    res.status(200).json({ projects });
+
+    res.status(200).json(projects);
   } catch (error) {
     console.log(error);
     next(error);
